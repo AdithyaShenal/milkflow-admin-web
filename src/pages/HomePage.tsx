f279fd37da3a5adb0131a54c488256a06f54f8c1
DNishadini <dinushikaherath150@gmail.com>
2026-01-05 10:15:29 +0530

    use StateCard component and create other cards

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,36 @@
+import StatCard from "../components/home/StatCard";
+const dashboardStats = {
+  todayLiters: 1240,
+  monthlyLiters: 28500,
+  avgPickups: 6.2,
+  pendingLiters: 420,
+};
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div className="p-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-4">
+        <StatCard
+          title="Total liters collected today"
+          value={`${dashboardStats.todayLiters} L`}
+        />
+
+        <StatCard
+          title="Total liters collected this month"
+          value={`${dashboardStats.monthlyLiters} L`}
+        />
+
+        <StatCard
+          title="Avg pickups per vehicle today"
+          value={dashboardStats.avgPickups}
+        />
+
+        <StatCard
+          title="Total production pending today"
+          value={`${dashboardStats.pendingLiters} L`}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
