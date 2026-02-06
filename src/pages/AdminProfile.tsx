36ae59124915ec42c9c6e161f41663b047333b04
DNishadini <dinushikaherath150@gmail.com>
2026-02-06 09:17:18 +0530

    Create header

---
+++ src/pages/AdminProfile.tsx
@@ -0,0 +1,26 @@
+import {
+  Pencil,
+  MapPin,
+  Shield,
+  User,
+  Mail,
+  Phone,
+  Calendar,
+} from "lucide-react";
+
+export default function ProfilePage() {
+  return (
+        <div className="min-h-screen bg-gray-50 px-6 py-8">
+      
+            <h1 className="text-3xl font-bold text-gray-800 mb-8">My Profile</h1>
+
+      
+      <div className="bg-white rounded-2xl shadow-sm border border-gray-100 p-6 flex flex-col md:flex-row md:items-center md:justify-between gap-6 mb-8">
+        <div className="flex items-center gap-5">
+          <img
+            src="https://i.pravatar.cc/150?img=3"
+            className="w-24 h-24 rounded-full object-cover ring-4 ring-orange-100"
+            alt="profile"
+          />
+  );
+}
